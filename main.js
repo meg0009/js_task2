d14c0dbd10629e2ac407c5b5dd23765a471859b7
Vasilii <megg0009@gmail.com>
2021-12-03 20:29:39 +0300

    разделил логику чтения данных, логику вычислений и логику вывода результатов на разные функции

--- main.js
+++ main.js
@@ -7,32 +7,41 @@ function initBody() {
 }
 
 function calculate() {
-	var a = Number(document.getElementsByName("A_coef")[0].value);
-	var b = Number(document.getElementsByName("B_coef")[0].value);
-	var c = Number(document.getElementsByName("C_coef")[0].value);
+	removeIllegalCoef();
 
-	var inputData = document.getElementById("input_data");
-	var newDiv = document.createElement("div");
-	newDiv.setAttribute("id", "illegal_coef");
-	var text = document.createTextNode("Не правильно введённые данные");
-	newDiv.appendChild(text);
+	var coefficients = getCoefficients();
+	var x1x2 = calculateX1X2(coefficients);
 
-	if (isNaN(a) || isNaN(b) || isNaN(c)) {
-		inputData.parentElement.insertBefore(newDiv, inputData.nextSibling);
+	if (typeof x1x2 === "string") {
+		insertIllegalCoef(x1x2);
 		return;
 	}
 
-	var illegalCoef = document.getElementById("illegal_coef");
-	if (illegalCoef != null) {
-		illegalCoef.parentElement.removeChild(illegalCoef);
+	addElementsIntoTable(coefficients, x1x2);
+	setX1X2(x1x2);
+}
+
+function getCoefficients() {
+	var a = Number(document.getElementsByName("A_coef")[0].value);
+	var b = Number(document.getElementsByName("B_coef")[0].value);
+	var c = Number(document.getElementsByName("C_coef")[0].value);
+	return {a: a, b: b, c: c};
+}
+
+function calculateX1X2(coefficients) {
+	var a = coefficients.a;
+	var b = coefficients.b;
+	var c = coefficients.c;
+
+	if (isNaN(a) || isNaN(b) || isNaN(c)) {
+		return "Не правильно введённые данные";
 	}
 
 	var x1 = 0;
 	var x2 = 0;
 
 	if (a === 0 && b === 0) {
-		inputData.parentElement.insertBefore(newDiv, inputData.nextSibling);
-		return;
+		return "Не правильно введённые данные";
 	} else if ((a === 0 || b === 0) && c === 0) {
 		x1 = 0;
 		if (a !== 0) {
@@ -49,24 +58,44 @@ function calculate() {
 			x2 = "";
 		} else if (b === 0) {
 			if (-c / a < 0) {
-				text.nodeValue = "Мнимые корни";
-				inputData.parentElement.insertBefore(newDiv, inputData.nextSibling);
-				return;
+				return "Мнимые корни";
 			}
 			x1 = Math.sqrt(-c / a);
 			x2 = -x1;
 		} else {
 			var d = b ** 2 - 4 * a * c;
 			if (d < 0) {
-				text.nodeValue = "Отрицательный дискриминант";
-				inputData.parentElement.insertBefore(newDiv, inputData.nextSibling);
-				return;
+				return "Отрицательный дискриминант";
 			}
 			x1 = (-b + Math.sqrt(d)) / (2 * a);
 			x2 = (-b - Math.sqrt(d)) / (2 * a);
 		}
 	}
+	return {x1: x1, x2: x2};
+}
 
+function insertIllegalCoef(str) {
+	var inputData = document.getElementById("input_data");
+	var newDiv = document.createElement("div");
+	newDiv.setAttribute("id", "illegal_coef");
+	var text = document.createTextNode(str);
+	newDiv.appendChild(text);
+	inputData.parentElement.insertBefore(newDiv, inputData.nextSibling);
+}
+
+function removeIllegalCoef() {
+	var illegalCoef = document.getElementById("illegal_coef");
+	if (illegalCoef != null) {
+		illegalCoef.parentElement.removeChild(illegalCoef);
+	}
+}
+
+function addElementsIntoTable(coefs, x1x2) {
+	var a = coefs.a;
+	var b = coefs.b;
+	var c = coefs.c;
+	var x1 = x1x2.x1;
+	var x2 = x1x2.x2;
 	var table = document.getElementById("results");
 	var newTr = document.createElement("tr");
 	newTr.addEventListener("click", removeElement, false)
@@ -92,14 +121,17 @@ function calculate() {
 	newTr.appendChild(newTdResX1);
 	newTr.appendChild(newTdResX2);
 	table.appendChild(newTr);
-	document.getElementsByName("x1")[0].value = String(x1);
-	document.getElementsByName("x2")[0].value = String(x2);
+}
+
+function setX1X2(x1x2) {
+	document.getElementsByName("x1")[0].value = String(x1x2.x1);
+	document.getElementsByName("x2")[0].value = String(x1x2.x2);
 }
 
 function zebra() {
 	var table = document.getElementsByTagName("tr");
 	for (var i = 2; i < table.length; i++) {
-		if (i % 2 === 0){
+		if (i % 2 === 0) {
 			table[i].setAttribute("class", "first_color");
 		} else {
 			table[i].setAttribute("class", "second_color");
